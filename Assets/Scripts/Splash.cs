53de8fc6e8af4e86de2b2053c05c79f03d2ed847
banshikRU <92977819+banshikRU@users.noreply.github.com>
2024-09-26 08:33:55 +0700

    main

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using dotmob;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    private void Start()
+    {
+        GameManager.LoadScene("MainMenu");
+    }
+    //private IEnumerator Start()
+    //{
+    //    if (!AdsManager.HaveSetupConsent)
+    //    {
+    //        SharedUIManager.ConsentPanel.Show();
+    //        yield return new WaitUntil(() => !SharedUIManager.ConsentPanel.Showing);
+    //    }
+
+    //    GameManager.LoadScene("MainMenu");
+    //}
+}
